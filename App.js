42ace09f4033cf9f3c41dee5d62c00fcebd624a4
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-03-08 15:33:37 +0100

    🚩 use native modules within react native

--- App.js
+++ App.js
@@ -1,114 +1,59 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  Alert,
+  NativeModules,
+  NativeEventEmitter,
+  Button,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// use fancy math native module
+const FancyMathEventEmitter = new NativeEventEmitter(NativeModules.FancyMath);
 
-const App: () => React$Node = () => {
+// use async sample native module
+const AsyncSampleEventEmitter = new NativeEventEmitter(
+  NativeModules.AsyncSample,
+);
+
+export default function App() {
+  const eventHandler = (result) => {
+    console.log('Event was fired with:', result);
+  };
+
+  useEffect(() => {
+    FancyMathEventEmitter.addListener('AddEvent', eventHandler);
+    AsyncSampleEventEmitter.addListener('GetStringEvent', eventHandler);
+    return () => {
+      FancyMathEventEmitter.removeAllListeners();
+      AsyncSampleEventEmitter.removeAllListeners();
+    };
+  }, []);
+
+  const onPressHandler = () => {
+    NativeModules.FancyMath.add(
+      NativeModules.FancyMath.Pi,
+      NativeModules.FancyMath.E,
+      function (result) {
+        Alert.alert(
+          'FancyMath',
+          `FancyMath says ${NativeModules.FancyMath.Pi} + ${NativeModules.FancyMath.E} = ${result}`,
+          [{text: 'OK'}],
+          {cancelable: false},
+        );
+      },
+    );
+  };
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{color: 'black', fontSize: 50}}>
+        FancyMath says PI = {NativeModules.FancyMath.Pi}
+      </Text>
+      <Text style={{color: 'black', fontSize: 50}}>
+        FancyMath says E = {NativeModules.FancyMath.E}
+      </Text>
+      <Button onPress={onPressHandler} title="Click me!" />
+    </View>
   );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+}
