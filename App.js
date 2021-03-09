acfafb84c30857b32dc757a8fed9bd9967b73440
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-03-09 09:55:59 +0100

    🛴 add two numbers

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
   Text,
@@ -6,6 +6,7 @@ import {
   NativeModules,
   NativeEventEmitter,
   Button,
+  TextInput,
 } from 'react-native';
 
 // use fancy math native module
@@ -17,10 +18,17 @@ const AsyncSampleEventEmitter = new NativeEventEmitter(
 );
 
 export default function App() {
+  // states
+  const [numberOne, setNumberOne] = useState('');
+  const [numberTwo, setNumberTwo] = useState('');
+  const [result, setResult] = useState('');
+
+  // this the event handler
   const eventHandler = (result) => {
     console.log('Event was fired with:', result);
   };
 
+  // on component did mount
   useEffect(() => {
     FancyMathEventEmitter.addListener('AddEvent', eventHandler);
     AsyncSampleEventEmitter.addListener('GetStringEvent', eventHandler);
@@ -30,6 +38,7 @@ export default function App() {
     };
   }, []);
 
+  // this is Click me! but on press handler
   const onPressHandler = () => {
     NativeModules.FancyMath.add(
       NativeModules.FancyMath.Pi,
@@ -45,6 +54,28 @@ export default function App() {
     );
   };
 
+  // this effect is used whenever the textinputs are changed
+  useEffect(() => {
+    addTwoNumbers();
+  }, [numberOne, numberTwo]);
+
+  // this function add two numbers and returns the result as a string
+  const addTwoNumbers = () => {
+    // check if inputs are empty to exit the function
+    if (numberOne === '' || numberTwo === '') {
+      setResult('');
+      return;
+    }
+    // native add operation
+    NativeModules.FancyMath.add(
+      parseFloat(numberOne),
+      parseFloat(numberTwo),
+      function (result) {
+        setResult(result.toString());
+      },
+    );
+  };
+
   return (
     <View
       style={{
@@ -53,13 +84,43 @@ export default function App() {
         alignItems: 'center',
         backgroundColor: 'white',
       }}>
-      <Text style={{color: 'black', fontSize: 50}}>
-        FancyMath says PI = {NativeModules.FancyMath.Pi}
-      </Text>
-      <Text style={{color: 'black', fontSize: 50}}>
-        FancyMath says E = {NativeModules.FancyMath.E}
-      </Text>
-      <Button onPress={onPressHandler} title="Click me!" />
+      <View style={{flex: 1}}>
+        <Text style={{color: 'black', fontSize: 50}}>
+          FancyMath says PI = {NativeModules.FancyMath.Pi}
+        </Text>
+        <Text style={{color: 'black', fontSize: 50}}>
+          FancyMath says E = {NativeModules.FancyMath.E}
+        </Text>
+        <Button onPress={onPressHandler} title="Click me!" />
+      </View>
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <TextInput
+          style={{
+            width: '20%',
+            height: '20%',
+            fontSize: 30,
+            borderRadius: 30,
+          }}
+          placeholder={'Number one'}
+          onChangeText={(text) => {
+            setNumberOne(text);
+          }}
+        />
+        <Text style={{color: 'black', fontSize: 50}}>+</Text>
+        <TextInput
+          style={{
+            width: '20%',
+            height: '20%',
+            fontSize: 30,
+            borderRadius: 30,
+          }}
+          placeholder={'Number two'}
+          onChangeText={(text) => {
+            setNumberTwo(text);
+          }}
+        />
+        <Text style={{color: 'black', fontSize: 50}}>= {result}</Text>
+      </View>
     </View>
   );
 }
